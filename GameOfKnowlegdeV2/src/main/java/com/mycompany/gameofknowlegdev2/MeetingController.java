867e6764e296661c9bcf2bb852b70ef1b321ee05
William Bolding <w.bolding@outlook.com>
2019-12-11 17:24:40 +0100

    Answers for africans

--- GameOfKnowlegdeV2/src/main/java/com/mycompany/gameofknowlegdev2/MeetingController.java
+++ GameOfKnowlegdeV2/src/main/java/com/mycompany/gameofknowlegdev2/MeetingController.java
@@ -45,6 +45,10 @@ public class MeetingController {
     private ImageView nationRep2;
     @FXML
     private ImageView nationRep3;
+    @FXML
+    private TextArea dialogTextArea1;
+    @FXML
+    private Button closeBtn;
 
     @FXML
     private void switchToEnd(ActionEvent event) {
@@ -99,7 +103,18 @@ public class MeetingController {
 
     @FXML
     private void printAnswerA(ActionEvent event) {
-        roomTextArea.setText(game.answerNPC("A"));
+        dialogTextArea1.setVisible(true);
+        closeBtn.setVisible(true);
+        dialogTextArea1.setText(
+                "To solve your problem, we would offer you the Photovoltaic panels.\n"
+                + "These are small and can upgrade some of your existing technology\n"
+                + "as well as i.e. provide enough energy to create a water heating\n"
+                + "system , for every home among many other quality of life changes.\n"
+                + "How it works practically is that the panels will be installed in\n"
+                + "every home, into walls and windows, so that every home has their\n"
+                + "own power supply. Which will be able to power their entire home.\n"
+                + "The catch with these is that they are quite cheap compared to other\n"
+                + "modern energy source, but of course the UN will also help financially.");
         if (dialogTextArea.getText().equals(game.talkNpc(new Command(CommandWord.TALK, "NR1")))) {
             game.getScore().setPoint(game.getScore().getPoint() + 1000);
         }
@@ -110,7 +125,18 @@ public class MeetingController {
 
     @FXML
     private void printAnswerB(ActionEvent event) {
-        roomTextArea.setText(game.answerNPC("B"));
+        dialogTextArea1.setVisible(true);
+        closeBtn.setVisible(true);
+        dialogTextArea1.setText(
+                "I have one possible solution for both your problems. We will be\n"
+                + "able to create both more jobs and the energy your village needs,\n"
+                + "by building a solar panel farm. Those need maintenance, which is\n"
+                + "a perfect opportunity to create jobs. Besides will the solar panel\n"
+                + "farm generate power proportional with the area it covers and cur-\n"
+                + "rently the average in the sub-Saharan region is 5-6 kWh/m2. Which means\n"
+                + "that there is a lot of energy just ready to be harvested. This increase\n"
+                + "in energy for your city will also make plenty more openings for\n"
+                + "different kinds of jobs, that usually has a high energy requirement.");
         if (dialogTextArea.getText().equals(game.talkNpc(new Command(CommandWord.TALK, "NR2")))) {
             game.getScore().setPoint(game.getScore().getPoint() + 1000);
         }
@@ -121,7 +147,21 @@ public class MeetingController {
 
     @FXML
     private void printAnswerC(ActionEvent event) {
-        roomTextArea.setText(game.answerNPC("C"));
+        dialogTextArea1.setVisible(true);
+        closeBtn.setVisible(true);
+        dialogTextArea1.setText(
+                "*You show him the solarpanelmodel*\n"
+                + "“Green energy” means energy that is sustainable for the environ-\n"
+                + "ment. An example of something that isn’t is the fossil energy that you\n"
+                + "mentioned yourself. These are polluting our atmosphere with toxic\n"
+                + "gasses, that induce climate changes in the world, which leads to\n"
+                + "severe problems all around the globe . Therefore, it is important\n"
+                + "that we deploy the green energy, which doesn’t pollute the atmos-\n"
+                + "phere. Besides that, it is also a lot more efficient than the kerosene,\n"
+                + "that you are currently using. So, what we can provide for you is\n"
+                + "a future proof solution, that helps the overall condition for everyone\n"
+                + "in your village, by either deploying small PV panels into homes or\n"
+                + "larger silicon panel farms. Depending on your needs.");
         if (dialogTextArea.getText().equals(game.talkNpc(new Command(CommandWord.TALK, "NR3")))) {
             game.getScore().setPoint(game.getScore().getPoint() + 1000);
         }
@@ -133,5 +173,11 @@ public class MeetingController {
     @FXML
     private void getCredScore(MouseEvent event) {
         credBar.setProgress(game.getCredScore().getCredScore());
+    }
+
+    @FXML
+    private void closeWindow(ActionEvent event) {
+         dialogTextArea1.setVisible(false);
+        closeBtn.setVisible(false);
     }
 }
